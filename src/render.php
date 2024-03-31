116f9ce0edc7ed1c96b5f641155129a545755092
Sam Seay <samueljseay@gmail.com>
2024-03-31 18:28:29 +0800

    Add todos to context.

--- src/render.php
+++ src/render.php
@@ -8,7 +8,7 @@ $todos = get_transient('my_todo_list');
 <div 
 	data-wp-interactive="iapi-todos"	
 	<?php echo get_block_wrapper_attributes(); ?>
-	<?php echo wp_interactivity_data_wp_context( array() ); ?>
+	<?php echo wp_interactivity_data_wp_context( array("todos" => $todos) ); ?>
 >
 	<p >
 		<?php if (!empty($todos)): ?>
@@ -23,6 +23,9 @@ $todos = get_transient('my_todo_list');
 									<?php echo esc_html($todo['description']); ?> - 
 									<strong><?php echo $todo['is_completed'] ? esc_html__('Completed', 'todolist') : esc_html__('Pending', 'todolist'); ?></strong> - 
 									Due: <?php echo esc_html($todo['due_date']); ?>
+
+									<label>Mark as completed: </label>
+									<input type="checkbox" <?php echo $todo['is_completed'] ? 'checked' : ''; ?> />
 							</li>
 					<?php endforeach; ?>
 			</ul>
