1b10675918c422fc73539bf171139e5db40ac3d5
Sam Seay <samueljseay@gmail.com>
2024-04-01 10:33:01 +0800

    Tidy up indentation of HTML in render.

--- build/render.php
+++ build/render.php
@@ -10,27 +10,25 @@ $todos = get_transient('my_todo_list');
 	<?php echo get_block_wrapper_attributes(); ?>
 	<?php echo wp_interactivity_data_wp_context( array("todos" => $todos) ); ?>
 >
-	<p >
-		<?php if (!empty($todos)): ?>
-			<ul>
-					<label>Add new todo:</label>
-					<input type="text" />
-					<input type="date" />
-					<button>Add</button>
-					
-					<?php foreach ($todos as $todo): ?>
-							<li>
-									<?php echo esc_html($todo['description']); ?> - 
-									<strong><?php echo $todo['is_completed'] ? esc_html__('Completed', 'todolist') : esc_html__('Pending', 'todolist'); ?></strong> - 
-									Due: <?php echo esc_html($todo['due_date']); ?>
+	<?php if (!empty($todos)): ?>
+		<ul>
+				<label>Add new todo:</label>
+				<input type="text" />
+				<input type="date" />
+				<button>Add</button>
+				
+				<?php foreach ($todos as $todo): ?>
+						<li>
+								<?php echo esc_html($todo['description']); ?> - 
+								<strong><?php echo $todo['is_completed'] ? esc_html__('Completed', 'todolist') : esc_html__('Pending', 'todolist'); ?></strong> - 
+								Due: <?php echo esc_html($todo['due_date']); ?>
 
-									<label>Mark as completed: </label>
-									<input type="checkbox" <?php echo $todo['is_completed'] ? 'checked' : ''; ?> />
-							</li>
-					<?php endforeach; ?>
-			</ul>
-		<?php else: ?>
-				<p><?php esc_html_e('No todos found.', 'todolist'); ?></p>
-		<?php endif; ?>	
-	</p>
+								<label>Mark as completed: </label>
+								<input type="checkbox" <?php echo $todo['is_completed'] ? 'checked' : ''; ?> />
+						</li>
+				<?php endforeach; ?>
+		</ul>
+	<?php else: ?>
+			<p><?php esc_html_e('No todos found.', 'todolist'); ?></p>
+	<?php endif; ?>
 </div>
